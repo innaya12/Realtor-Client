d6dc6a6f11795917256fd719c8588e962bd02c17
wecode <yakubov.inna2@gmail.com>
2020-01-18 16:20:49 +0200

    added design to homepage

---
+++ realtorApp/src/componentsInUse/navBar/buildNavbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+class BuildNavbar extends React.Component {
+
+    render() {
+        const {label, index, innerMenu, changeActiveItem, activeItem} = this.props;
+        return (
+            <li onMouseOver={() => changeActiveItem(index)}>
+                {index === 0 && //linked before to cities need to change to the new updated title
+                <p><Link to="/" style={{color: "black", textDecoration: "none"}}>Buy</Link></p>}
+                {index === 1 && <p>{label}</p>}
+                {/* {index === 1 && <p><Link to="/sell" style={{color: "black", textDecoration: "none"}}>{label}</Link></p>} */}
+                {/* {index === 2 && <p><Link to="/rent" style={{color: "black", textDecoration: "none"}}>{label}</Link></p>} */}
+                {index === 2 && <p>{label}</p>}
+                {index !== 1 && index !== 2 && index !== 0 && <p>{label}</p>}
+
+            </li>
+        )
+    }
+}
+
+export default BuildNavbar;
+
--- realtorApp/src/data/getData/getApartments.jsx
+++ realtorApp/src/data/getData/getApartments.jsx
@@ -1,9 +1,9 @@
 const axios = require('axios');
 
-const getApartment = (handleSuccess) => {
-    axios.get("http://localhost:8000/apartments")
-    .then(result => handleSuccess(result.data))
-}
+// const getApartment1 = (handleSuccess) => {
+//     axios.get("http://localhost:8000/apartments")
+//     .then(result => handleSuccess(result.data))
+// }
 
 const getApartmentByID = (handleSuccess, apartmentId) => {
     axios.get(`http://localhost:8000/apartments/${apartmentId}`)
@@ -14,17 +14,16 @@ const getApartmentByID = (handleSuccess, apartmentId) => {
 
 
 
-export {getApartmentByID, getApartment};
-
-
-
 
 // fake data for future test - to active remove '1' from name
-    // const getApartmentDataFromServer1= (handleSuccess) => {
-    //     fetch("https://storage.googleapis.com/realtour/apartments-rt.json", {
-    //             method: 'GET',
-    //         }
-    //     ).then(response => response.json()
-    //     ).then(success => handleSuccess(success)
-    //     ).catch(error => console.log(error))
-    // };
+    const getApartment = (handleSuccess) => {
+        fetch("https://storage.googleapis.com/realtour/apartments-rt.json", {
+                method: 'GET',
+            }
+        ).then(response => response.json()
+        ).then(success => handleSuccess(success)
+        ).catch(error => console.log(error))
+    };
+
+export {getApartmentByID, getApartment};
+
